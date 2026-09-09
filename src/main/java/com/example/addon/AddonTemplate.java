86db49747248b0aa2f49bf0cce00832ba495d001
sonoiojenzo-crypto <sonoiojenzo@gmail.com>
2026-09-09 05:37:16 +0200

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -2,7 +2,8 @@ package com.example.addon;
 
 import com.example.addon.commands.CommandExample;
 import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
+import com.example.addon.modules.AutoSheepFarm;
+import com.example.addon.modules.SheepEsp;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
@@ -15,15 +16,16 @@ import org.slf4j.Logger;
 
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+    public static final Category CATEGORY = new Category("Progetto Becuru");
+    public static final HudGroup HUD_GROUP = new HudGroup("Progetto Becuru");
 
     @Override
     public void onInitialize() {
         LOG.info("Initializing Meteor Addon Template");
 
         // Modules
-        Modules.get().add(new ModuleExample());
+        Modules.get().add(new SheepEsp());
+        Modules.get().add(new AutoSheepFarm());
 
         // Commands
         Commands.add(new CommandExample());
