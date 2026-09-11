2a4be2fdf90df381601fbdf3c113c08c7e5bde14
sonoiojenzo-crypto <sonoiojenzo@gmail.com>
2026-09-11 02:26:29 +0200

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -2,8 +2,7 @@ package com.example.addon;
 
 import com.example.addon.commands.CommandExample;
 import com.example.addon.hud.HudExample;
-import com.example.addon.modules.AutoSheepFarm;
-import com.example.addon.modules.SheepEsp;
+import com.example.addon.modules.ModuleExample;
 import com.mojang.logging.LogUtils;
 import meteordevelopment.meteorclient.addons.GithubRepo;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
@@ -16,17 +15,15 @@ import org.slf4j.Logger;
 
 public class AddonTemplate extends MeteorAddon {
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Progetto Becuru");
-    public static final HudGroup HUD_GROUP = new HudGroup("Progetto Becuru");
+    public static final Category CATEGORY = new Category("Example");
+    public static final HudGroup HUD_GROUP = new HudGroup("Example");
 
     @Override
     public void onInitialize() {
         LOG.info("Initializing Meteor Addon Template");
 
         // Modules
-        Modules.get().add(new SheepEsp());
-        Modules.get().add(new AutoSheepFarm());
-        Modules.get().add(new ShopTracker());
+        Modules.get().add(new ModuleExample());
 
         // Commands
         Commands.add(new CommandExample());
